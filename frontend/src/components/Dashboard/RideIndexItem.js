dda3950a1b18de14e0b7434d14ef6fb9267c067a
Michael <mmoses1127@gmail.com>
2022-10-28 16:35:41 -0700

    prep for deployment

--- frontend/src/components/Dashboard/RideIndexItem.js
+++ frontend/src/components/Dashboard/RideIndexItem.js
@@ -2,14 +2,12 @@ import smallLogo from '../../assets/small_logo.svg';
 
 
 const RideIndexItem = ({ride}) => {
-  console.log(ride)
-
 
   return (
     <div className="ride-index-item">
       <div className="ride-item-header">
         <img className="avatar-image" src={smallLogo} alt={ride.athleteId} />
-        
+
       </div>
       <div className="ride-item-stats">
         
