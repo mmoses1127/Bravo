02fc2f45e533ea9038e3e3782a32a65a3a6f3eb6
Michael <mmoses1127@gmail.com>
2022-10-28 16:17:09 -0700

    prepare for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
     resource :session, only: [:show, :create, :destroy]
     resources :rides, only: [:index, :show, :create, :update, :destroy]
   end
+
+  get '*path', to: "static_pages#frontend_index"
+  
 end
