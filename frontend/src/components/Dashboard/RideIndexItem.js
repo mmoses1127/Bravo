dd0c1d94b7a1ffe06bf83c0691f367f8564c55c1
Michael <mmoses1127@gmail.com>
2022-10-30 16:24:53 -0700

    work on html and css for feed cards

--- frontend/src/components/Dashboard/RideIndexItem.js
+++ frontend/src/components/Dashboard/RideIndexItem.js
@@ -7,7 +7,7 @@ import { getRide } from '../../store/rides';
 const RideIndexItem = ({ride}) => {
   const dispatch = useDispatch();
   const athlete_id = ride.athlete_id;
-  const athlete = useSelector((state) => state.users[athlete_id]);
+  // const athlete = useSelector((state) => state.users[athlete_id]);
 
   // useEffect(() => {
   // }, [])
@@ -15,19 +15,52 @@ const RideIndexItem = ({ride}) => {
   return (
     <div className="feed-card">
       <div className="card-header">
-        <img className="avatar-image" src={smallLogo} alt={ride.athleteId} />
+        <img className="avatar-image" src={smallLogo} alt="Avatar" />
         <div className="card-header-text">
           <h5>Athlete Name</h5>
           <br></br>
-          <p>Realtive Time | Location</p>
+          <p>Relative Time | Location</p>
         </div>
-
       </div>
       <div className="card-body">
-        
+        <div className="activity-symbol">
+          <i class="fa-light fa-bicycle"></i>
+        </div>
+        <div className="card-body-main">
+          <h3>Ride Title</h3>
+          <div className="card-body-text">
+            <ul className="stats">
+              <li>
+                <p>Distance</p>
+                <h4>25.0</h4>
+              </li>
+              <li>
+                <p>Elev Gain</p>
+                <h4>300 m</h4>
+              </li>
+              <li>
+                <p>Time</p>
+                <h4>2h 9m</h4>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="card-media">
-        
+        <div className="map-container">
+
+        </div>
+        <div className="photo-container">
+          <div className="secondary-photo">
+
+          </div>
+          <div className="secondary-photo">
+            
+          </div>
+          <div className="secondary-photo">
+            
+          </div>
+        </div>
       </div>
       <div className="card-footer">
         
