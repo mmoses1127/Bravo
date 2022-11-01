faff76c16225a79fdb192c72bb4e2b4b62fc5915
Michael <mmoses1127@gmail.com>
2022-11-01 14:54:55 -0700

    failed aws setup, started mapbox setup

---
+++ frontend/src/components/Map/Map.js
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoibW1vc2VzMTEyNyIsImEiOiJjbDl5cWtkdnAwN2pwM3BrbnZsNTZzZHIzIn0.5DYp57TWNGkULiO3KhdVbg';
+
+const Map = () => {
+
+};
+
+export default Map;
