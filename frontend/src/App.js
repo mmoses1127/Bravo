319c572f434bc21e93d0223f41354c0199efc74b
Michael <mmoses1127@gmail.com>
2022-11-01 15:04:15 -0700

    complete basic map tutorial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Dashboard from './components/Dashboard';
 import RideForm from './components/RideForm';
 import RideShow from './components/RideShow';
 import RideEdit from './components/RideEdit';
+import Map from './components/Map/Map';
 
 
 function App() {
@@ -35,6 +36,9 @@ function App() {
         </Route>        
         <Route path="/rides/:rideId">
           <RideShow/>
+        </Route>
+        <Route path="/map">
+          <Map/>
         </Route>
       </Switch>
     </>
--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -5,6 +5,28 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibW1vc2VzMTEyNyIsImEiOiJjbDl5cWtkdnAwN2pwM3Brb
 
 const Map = () => {
 
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [lng, lat],
+    zoom: zoom
+    });
+  });
+
+  return (
+    <div>
+    <div ref={mapContainer} className="map-container" />
+    </div>
+    );
+
 };
 
 export default Map;
