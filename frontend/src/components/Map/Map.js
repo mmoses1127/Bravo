ef83d92f84235ff151fa2679a4f93645a7093393
Michael <mmoses1127@gmail.com>
2022-11-01 15:18:49 -0700

    render map to app

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
+import './Map.css';
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibW1vc2VzMTEyNyIsImEiOiJjbDl5cWtkdnAwN2pwM3BrbnZsNTZzZHIzIn0.5DYp57TWNGkULiO3KhdVbg';
 
 const Map = () => {
@@ -22,7 +23,7 @@ const Map = () => {
   });
 
   return (
-    <div>
+    <div className="outer-map-container">
     <div ref={mapContainer} className="map-container" />
     </div>
     );
