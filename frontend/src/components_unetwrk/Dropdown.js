c5cd43537263367d3751f08ad6dc462297459a31
Michael <mmoses1127@gmail.com>
2024-01-28 15:06:30 -0500

    finish dropdown

--- frontend/src/components_unetwrk/Dropdown.js
+++ frontend/src/components_unetwrk/Dropdown.js
@@ -4,24 +4,18 @@ import { useHistory } from "react-router-dom";
 
 const Dropdown = ({tiers = [], columnOrder, setColumnOrder}) => {
 
+  
   const matchTier = (columnOrder) => {
-    if (columnOrder) {
-      const targetTier = tiers.filter(tier => tier.id === columnOrder);
-      setTargetTier(targetTier);
+    if (columnOrder != undefined) {
+      const targetTier = tiers.filter(tier => tier.position === columnOrder);
+
+      return targetTier[0];
     }
   };
-
-  const [showMenu, setShowMenu] = useState(false);
-  console.log('tiers', {tiers}.tiers);
-  const [targetTier, setTargetTier] = useState(columnOrder ? matchTier(columnOrder) : []);
-
-
-  useEffect(() => {
-    setTargetTier(matchTier(columnOrder));
-  }, [columnOrder])
-
   
-
+  const [showMenu, setShowMenu] = useState(false);
+  const [targetTier, setTargetTier] = useState(columnOrder ? matchTier(columnOrder) : []);
+  
   const openMenu = () => {
     setShowMenu(true);
   };
@@ -32,21 +26,22 @@ const Dropdown = ({tiers = [], columnOrder, setColumnOrder}) => {
 
   const handleChooseTier = (e) => {
     e.preventDefault();
-    console.log('set column');
-    setColumnOrder(e.target.value);
+    let targetPosition = parseInt(e.target.getAttribute('position'));
+    setColumnOrder(targetPosition);
+    setTargetTier(matchTier(targetPosition));
+    closeMenu();
   }
-
 
 
   return (
     <div onMouseEnter={openMenu} onMouseLeave={closeMenu} className="profile-drop-down, nav-dropdown">
       <div className='flex flex-row w-full justify-between'>
-        <h4>{targetTier.length ? targetTier[0].name : `Select Column`}</h4>
+        <h4>{targetTier.name ? targetTier.name : `Select Column`}</h4>
         <i className="fa-solid fa-angle-down"></i>
       </div>
       <ul className='dropdown-list'>
       {showMenu && tiers.map(tier => (
-          <li key={tier.id} className="dropdown-item" onClick={handleChooseTier}>{tier.name}</li>
+          <li key={tier.id} position={tier.position} className="dropdown-item" onClick={handleChooseTier}>{tier.name}</li>
       ))}
       </ul>      
     </div>
